c8086e95becb91655ada733d4502cb4361c31495
i10416 <ito.yo16uh90616@gmail.com>
2026-02-11 05:01:43 +0900

    feat: btree/search

--- crates/btree/src/lib.rs
+++ crates/btree/src/lib.rs
@@ -1,29 +1,49 @@
-use std::fmt::Display;
+use std::{cell::RefCell, fmt::Display, rc::Rc};
+
+type Link<T> = Option<Rc<RefCell<T>>>;
 
 pub struct BTree {
     order: usize,
-    root: Option<Node>,
+    root: Link<Node>,
 }
 
 impl BTree {
     pub fn new(order: usize) -> Self {
-        let root = Node::Leaf {
+        let root = Node {
             keys: vec![],
             values: vec![],
+            parent: None,
+            children: vec![],
+            is_leaf: true,
         };
         Self {
             order,
-            root: Some(root),
+            root: Some(Rc::new(RefCell::new(root))),
         }
     }
+    pub fn new_with_internal(order: usize) -> Self {
+        let root = Node {
+            keys: vec![],
+            values: vec![],
+            parent: None,
+            children: vec![],
+            is_leaf: false,
+        };
+        Self {
+            order,
+            root: Some(Rc::new(RefCell::new(root))),
+        }
+    }
+
     pub fn order(&self) -> usize {
         self.order
     }
-    pub fn root(&self) -> Option<&Node> {
-        self.root.as_ref()
-    }
+
     pub fn search(&self, key: i32) -> Option<String> {
-        None
+        match self.root.as_ref() {
+            None => None,
+            Some(root) => root.borrow().search(key),
+        }
     }
 }
 
@@ -33,16 +53,97 @@ impl Display for BTree {
     }
 }
 
-pub enum Node {
-    // A leaf node carrying keys and associated values.
-    Leaf { keys: Vec<i32>, values: Vec<String> },
-    Internal { keys: Vec<i32>, values: Vec<String> },
+pub struct Node {
+    /// sorted keys
+    keys: Vec<i32>,
+    values: Vec<String>,
+    parent: Link<Node>,
+    children: Vec<Box<Node>>,
+    is_leaf: bool,
+}
+
+impl Node {
+    pub fn leaf<S: AsRef<str>>(keys: &[i32], values: &[S]) -> Self {
+        Node {
+            keys: keys.to_vec(),
+            values: values
+                .iter()
+                .map(|item| item.as_ref().to_string())
+                .collect(),
+            parent: None,
+            children: vec![],
+            is_leaf: true,
+        }
+    }
+    pub fn leaf_with_parent<S: AsRef<str>>(
+        keys: &[i32],
+        values: &[S],
+        parent: Rc<RefCell<Node>>,
+    ) -> Self {
+        Node {
+            keys: keys.to_vec(),
+            values: values
+                .iter()
+                .map(|item| item.as_ref().to_string())
+                .collect(),
+            parent: Some(parent),
+            children: vec![],
+            is_leaf: true,
+        }
+    }
+    pub fn keys(&self) -> &[i32] {
+        match self {
+            Node { keys, .. } => keys,
+        }
+    }
+
+    pub fn insert_keys(&mut self, new_keys: &[i32]) {
+        match self {
+            Node { keys, .. } => keys.extend(new_keys),
+        }
+    }
+    pub fn insert_values(&mut self, new_values: &[String]) {
+        match self {
+            Node { values, .. } => {
+                values.extend_from_slice(new_values);
+            }
+        }
+    }
+    pub fn insert_children(&mut self, new_children: Vec<Box<Node>>) {
+        match self {
+            Node { children, .. } => {
+                children.extend(new_children);
+            }
+        }
+    }
+    pub fn search(&self, key: i32) -> Option<String> {
+        let node = self;
+        let keys = node.keys();
+        let idx = keys.binary_search(&key);
+        match idx {
+            Ok(idx) => match &*node {
+                Node { values, .. } => Some(values[idx].clone()),
+            },
+            Err(idx) => match &*node {
+                Node {
+                    children,
+                    is_leaf: false,
+                    ..
+                } => children[idx].search(key),
+                _ => None,
+            },
+        }
+    }
 }
 
 impl Display for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Node::Leaf { keys, .. } => write!(
+            Node {
+                keys,
+                is_leaf: true,
+                ..
+            } => write!(
                 f,
                 "[Leaf [{}]]",
                 keys.into_iter()
@@ -50,7 +151,11 @@ impl Display for Node {
                     .collect::<Vec<_>>()
                     .join(" ")
             ),
-            Node::Internal { keys, .. } => write!(
+            Node {
+                keys,
+                is_leaf: false,
+                ..
+            } => write!(
                 f,
                 "[Internal [{}]]",
                 keys.into_iter()
@@ -64,21 +169,23 @@ impl Display for Node {
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Deref;
+
     use crate::Node;
 
     #[test]
     fn render_leaf_node() {
-        let node = crate::Node::Leaf {
-            keys: vec![1, 2, 3],
-            values: vec!["a".to_string(), "b".to_string(), "c".to_string()],
-        };
+        let node = crate::Node::leaf(&[1, 2, 3], &["a", "b", "c"]);
         assert_eq!(format!("{node}"), "[Leaf [1 2 3]]")
     }
     #[test]
     fn render_internal_node() {
-        let node = crate::Node::Internal {
+        let node = crate::Node {
             keys: vec![10, 20],
-            values: vec!["a".to_string(), "b".to_string(), "c".to_string()],
+            children: vec![],
+            is_leaf: false,
+            values: vec!["a".to_string(), "b".to_ascii_lowercase()],
+            parent: None,
         };
         assert_eq!(format!("{node}"), "[Internal [10 20]]")
     }
@@ -102,12 +209,84 @@ mod tests {
     #[test]
     fn select_root_from_empty_btree() {
         let btree = crate::BTree::new(3);
-        assert!(matches!(btree.root(), Some(Node::Leaf { .. })))
+        assert!(matches!(
+            btree.root.unwrap().borrow().deref(),
+            Node { is_leaf: true, .. }
+        ))
     }
     #[test]
     fn search_value_from_empty_btree() {
         let btree = crate::BTree::new(3);
         let item = btree.search(10);
         assert!(matches!(item, None))
+    }
+    #[test]
+    fn search_value_from_single_level_btree() {
+        let btree = crate::BTree::new(3);
+        btree
+            .root
+            .as_ref()
+            .unwrap()
+            .borrow_mut()
+            .insert_keys(&[10, 20, 30]);
+        btree.root.as_ref().unwrap().borrow_mut().insert_values(&[
+            "a".to_string(),
+            "b".to_string(),
+            "c".to_string(),
+        ]);
+        let item = btree.search(10);
+        assert!(matches!(item.as_deref(), Some("a")));
+        let item = btree.search(20);
+        assert!(matches!(item.as_deref(), Some("b")));
+        let item = btree.search(30);
+        assert!(matches!(item.as_deref(), Some("c")));
+        let item = btree.search(5);
+        assert!(matches!(item.as_deref(), None));
+        let item = btree.search(25);
+        assert!(matches!(item.as_deref(), None));
+        let item = btree.search(35);
+        assert!(matches!(item.as_deref(), None));
+    }
+    #[test]
+    fn search_value_from_multiple_level_btree() {
+        let mut btree = crate::BTree::new_with_internal(3);
+        btree
+            .root
+            .as_mut()
+            .unwrap()
+            .borrow_mut()
+            .insert_keys(&[10, 20]);
+        let child1 = Box::new(crate::Node::leaf_with_parent(
+            &[1, 5, 9],
+            &["a", "b", "c"],
+            btree.root.clone().unwrap(),
+        ));
+        let child2 = Box::new(crate::Node::leaf_with_parent(
+            &[15, 18, 19],
+            &["d", "e", "f"],
+            btree.root.clone().unwrap(),
+        ));
+        let child3 = Box::new(crate::Node::leaf_with_parent(
+            &[25, 28, 30],
+            &["g", "h", "i"],
+            btree.root.clone().unwrap(),
+        ));
+        btree
+            .root
+            .as_mut()
+            .unwrap()
+            .borrow_mut()
+            .insert_children(vec![child1, child2, child3]);
+
+        let item = btree.search(1);
+        assert!(matches!(item.as_deref(), Some("a")));
+        let item = btree.search(18);
+        assert!(matches!(item.as_deref(), Some("e")));
+        let item = btree.search(28);
+        assert!(matches!(item.as_deref(), Some("h")));
+        let item = btree.search(22);
+        assert!(matches!(item.as_deref(), None));
+        let item = btree.search(100);
+        assert!(matches!(item.as_deref(), None));
     }
 }
