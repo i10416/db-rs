2a90760b08845eaeea12fc9b4d1bb015871632df
i10416 <ito.yo16uh90616@gmail.com>
2026-05-02 19:46:03 +0900

    tweak: move nom specific impl to a module

--- crates/naivedb-api/src/lib.rs
+++ crates/naivedb-api/src/lib.rs
@@ -1,52 +1,13 @@
-use nom::{
-    IResult, Parser,
-    bytes::complete::{tag_no_case, take_while1},
-    character::complete::multispace1,
-};
+pub mod parsing;
 
 #[derive(Debug, PartialEq)]
 pub enum Query {
     Select(SelectStatement),
 }
 
-type RawSpan<'a> = &'a str;
-
-type ParseResult<'a, T> = IResult<RawSpan<'a>, T>;
-
 #[derive(Debug, PartialEq)]
 pub struct SelectStatement {
     // always select all for now
     // cols: ...,
     pub table: String, // only one table is allowed for from clause
-}
-pub fn parse<'a>(s: RawSpan<'a>) -> ParseResult<'a, Query> {
-    (
-        tag_no_case("select"),
-        multispace1,
-        nom::character::char('*'),
-        multispace1,
-        tag_no_case("from"),
-        multispace1,
-        identifier,
-    )
-        .map(|(_, _, _, _, _, _, ident)| Query::Select(SelectStatement { table: ident }))
-        .parse(s)
-}
-
-pub(crate) fn identifier<'a>(i: RawSpan<'a>) -> ParseResult<'a, String> {
-    take_while1(|c: char| c.is_alphanumeric())
-        .map(|a: &str| a.to_string())
-        .parse(i)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{Query, SelectStatement, parse};
-
-    #[test]
-    fn parse_select() {
-        let q = "SELECT * FROM t1";
-        let (_, q) = parse(q).unwrap();
-        assert_eq!(q, Query::Select(SelectStatement { table: "t1".into() }))
-    }
 }
---
+++ crates/naivedb-api/src/parsing/nom.rs
@@ -0,0 +1,43 @@
+use nom::{
+    IResult, Parser,
+    bytes::complete::{tag_no_case, take_while1},
+    character::complete::multispace1,
+};
+
+use crate::{Query, SelectStatement};
+
+pub fn parse<'a>(s: RawSpan<'a>) -> ParseResult<'a, Query> {
+    (
+        tag_no_case("select"),
+        multispace1,
+        nom::character::char('*'),
+        multispace1,
+        tag_no_case("from"),
+        multispace1,
+        identifier,
+    )
+        .map(|(_, _, _, _, _, _, ident)| Query::Select(SelectStatement { table: ident }))
+        .parse(s)
+}
+
+pub(crate) fn identifier<'a>(i: RawSpan<'a>) -> ParseResult<'a, String> {
+    take_while1(|c: char| c.is_alphanumeric())
+        .map(|a: &str| a.to_string())
+        .parse(i)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Query, SelectStatement, parsing::nom::parse};
+
+    #[test]
+    fn parse_select() {
+        let q = "SELECT * FROM t1";
+        let (_, q) = parse(q).unwrap();
+        assert_eq!(q, Query::Select(SelectStatement { table: "t1".into() }))
+    }
+}
+
+pub type RawSpan<'a> = &'a str;
+
+pub type ParseResult<'a, T> = IResult<RawSpan<'a>, T>;
